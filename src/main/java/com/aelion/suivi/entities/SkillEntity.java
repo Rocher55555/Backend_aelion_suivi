c020c5e647ad2fd03cfa99b83b5577ed36e37e47
pia <pia.andreotti@gmail.com>
2022-09-13 17:04:41 +0200

    CREATION of Skill entity, repository, service and controller. It will be link with internEvaluation

---
+++ src/main/java/com/aelion/suivi/entities/SkillEntity.java
@@ -0,0 +1,68 @@
+/**
+ * 
+ */
+package com.aelion.suivi.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+/**
+ * @author Aelion
+ * POJO
+ */
+
+@Entity
+@Table (name = "skill")
+public class SkillEntity {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(unique=true, nullable=false, length=150)
+	private String title;  //colonne
+	
+	private String description;
+
+	
+	
+	/**
+	 * @return the title
+	 */
+	public String getTitle() {
+		return title;
+	}
+	/**
+	 * @param title the title to set
+	 */
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+	/**
+	 * @param description the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
+
+
+}
