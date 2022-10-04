87f089cead6799d2de95cd1bae90e625170395c5
pia <pia.andreotti@gmail.com>
2022-10-04 14:09:11 +0200

    CREATION: UserEntity

---
+++ src/main/java/com/aelion/suivi/entities/UserEntity.java
@@ -0,0 +1,32 @@
+package com.aelion.suivi.entities;
+
+public class UserEntity {
+	private Long id;
+	private String email;
+	
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
